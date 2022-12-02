2192c9d3f970433ca45b94840751dadcf5e6606c
Arnold Okkel <arnoldokkel121@gmail.com>
2022-12-02 13:57:08 +0100

    final answer Day1

--- day 1 AoC/solution.py
+++ day 1 AoC/solution.py
@@ -1,19 +1,12 @@
-Main()
-        {
-            int Wert1 = 0;
-            Ergebnisse[];
-            Daten[] = File.ReadAllText("data.txt",ReadAllText)
-            foreach (var Calorien in Daten)
-            {
-              Wert1 += Calorien;
-                if(Calorien == 0 || Calorien == NULL)
-                {
-                 int i=0;
-                 Ergebnisse[i] = Wert1;
-                 i++;
-                 Wert1 = 0;
-                }
-            }
-            Console.WriteLine("%1",Ergebnisse.Max());
-            Console.ReadLine();
-            return 0;
+data = open ("day 1 AoC\data.txt").read().split("\n")
+wert1 = int(0)
+ergebnisse = []
+for calorien in data:
+    if calorien == "":
+        ergebnisse.append(int(wert1))
+        wert1 = 0
+    else: wert1 += int(calorien)
+print(f"Most calories: {max(ergebnisse)}")
+ergebnisse.sort(reverse=True)
+top_three = ergebnisse[0] + ergebnisse[1] +ergebnisse[2]
+print(f"Top three total calories: {top_three}")
