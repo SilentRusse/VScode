c8da18b2ccfe90af6e81f8b1677a205b432bc02c
Arnold Okkel <arnoldokkel121@gmail.com>
2022-12-06 16:34:18 +0100

    day 5 different approach day 6 AoC W.I.P

--- day 5 AoC/solution.py
+++ day 5 AoC/solution.py
@@ -1,15 +1,20 @@
 import re
 import string
+import collections
 stacks = open("day 5 AoC\data.txt").read().split("\n")
-container = []
-stack_number = []
 instructions = []
-index_number = []
+container = []
+mylist = []
+row_number = []
 for line in stacks:
-    if string.ascii_uppercase in line:
-        container.append()
-    print(container)
-    if "move" in line:
-        instructions.append(find_number)
-    else:
-        stack_number.append(find_number)
+    if 'move' in line:
+        instructions.append(re.findall("[0-9]",line))
+    else: 
+        container.append(list(enumerate(line)))
+count = 0
+for item in container:
+    for letter in item:
+        if type(letter[0]) == int and letter[1].isalpha():
+            mylist[letter[0]].append(letter[1])
+        elif type(letter[0]) == int and type(letter[1]) == int:
+            mylist[letter[0]].append([])
---
+++ day 6 AoC/solution.py
@@ -0,0 +1,6 @@
+from collections import Counter
+data = open("day 6 AoC\data.txt").read()
+sequence = ""
+for i in len(data):
+    sequence += i
+    
