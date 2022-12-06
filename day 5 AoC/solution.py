4954dd088ef05b3765143949575483e3573e02de
Arnold Okkel <arnoldokkel121@gmail.com>
2022-12-06 20:59:36 +0100

    W.I.P

--- day 5 AoC/solution.py
+++ day 5 AoC/solution.py
@@ -4,17 +4,28 @@ import collections
 stacks = open("day 5 AoC\data.txt").read().split("\n")
 instructions = []
 container = []
-mylist = []
+mylist = [[],[],[],[],[],[],[],[]]
 row_number = []
 for line in stacks:
+    for i in range(1,len(line),4):
+        if line[i].isalpha() and 'move' not in line:
+            mylist.append(str(line[i]))
+        elif line[i].isnumeric() and 'move' not in line:
+            mylist[i].append(int(line[i]))
+        elif not line[i].isalpha():
+            break
+        #if type(line[i]) == int:
+         #   mylist.append([line[i]])
     if 'move' in line:
         instructions.append(re.findall("[0-9]",line))
-    else: 
-        container.append(list(enumerate(line)))
 count = 0
+print(mylist)
+print(row_number)
+"""
 for item in container:
     for letter in item:
         if type(letter[0]) == int and letter[1].isalpha():
-            mylist[letter[0]].append(letter[1])
-        elif type(letter[0]) == int and type(letter[1]) == int:
-            mylist[letter[0]].append([])
+            mylist.append(letter[1])
+        #elif type(letter[0]) == int and type(letter[1]) == int:
+            #mylist[letter[1]].append([])
+"""
--- day 6 AoC/solution.py
+++ day 6 AoC/solution.py
@@ -1,6 +1,16 @@
 from collections import Counter
 data = open("day 6 AoC\data.txt").read()
 sequence = ""
-for i in len(data):
-    sequence += i
-    
+Position = 0
+laenge = len(data)
+unique = True
+for i in range(0,laenge):
+    sequence += data[i]
+    if(len(sequence) <= 4):
+        print(Counter(sequence))
+        print("\n")
+    elif len(sequence) > 4 and unique == True:
+        test = sequence[len(sequence) - 4:]
+        Counter(test)
+        #print(test)
+        #print("\n")
