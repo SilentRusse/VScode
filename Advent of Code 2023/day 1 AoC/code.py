ce9ae4b7a5cfdc8027b44f19aa11331f162d741c
SilentRusse <119666352+SilentRusse@users.noreply.github.com>
2023-12-06 10:18:10 +0100

    Part One done with correct solution

--- Advent of Code 2023/day 1 AoC/code.py
+++ Advent of Code 2023/day 1 AoC/code.py
@@ -1,10 +1,16 @@
-import re
-file = open ("\Advent of Code 2023\day 1 AoC\day1.txt").read().split("\n")
-allnumbers = []
+import numbers
+file = open ('Advent of Code 2023\day 1 AoC\day1.txt').read().split('\n')
+calibration = []
 summary = []
 for line in file:
-    allnumbers =re.findall(r'\b\d+\b', line)
-    last = len(allnumbers)
-    summary.append = allnumbers[0] + allnumbers[last]
-sum_of_calibration = sum(summary)
-print (f"the sum is:{sum_of_calibration}")
+    numbers_in_line = []
+    for letter in line:
+        if letter.isdigit() == True:
+            numbers_in_line.append(letter)
+    calibration.append(numbers_in_line[0] + numbers_in_line[-1])
+    del numbers_in_line
+calibration = list(map(int, calibration))
+print(sum(calibration))
+#Part One of the Challange is done
+
+    
