60761902b38774245e19f2c425baac7ab10b8af9
Arnold Okkel <arnoldokkel121@gmail.com>
2024-12-07 08:28:03 +0100

    I am short of approx. 30 safe reports . . . why does my code not work correctly ??

--- Advent of Code 2024/Day 2/solution.py
+++ Advent of Code 2024/Day 2/solution.py
@@ -1,23 +1,21 @@
-def is_ascending(lst):
-    return all(
-        lst[index] <= lst[index+1]
-        for index in range(len(lst) - 1)
-    )
-def is_descending(lst):
-    return all(
-        lst[index] >= lst[index+1]
-        for index in range(len(lst) - 1)
-    )
 difference_per_level=[]
 safe_reports = 0
+
+def isIncreasing(list):
+    return all(list[index] < list[index + 1] for index in range(0,len(list) - 1))
+
+def isDecreasing(list):
+    return all(list[index] > list[index + 1] for index in range(0,len(list) - 1))
+
 input_file=open("input.txt","r")
-for report in input_file:
-    report_list= report.rstrip('\n').split(" ")
-    for x in range(0,len(report_list)-1):
-        level_1 = int(report_list[x])
-        level_2 = int(report_list[x + 1])
-        absolut_diff = level_1 - level_2
-        difference_per_level.append(absolut_diff)
-    if (is_ascending(difference_per_level) == True or is_descending(difference_per_level) == True) and (difference_per_level in range(1,3) or difference_per_level in range(-1,-3)):
-        safe_reports =+ 1
-print(safe_reports)
+for report_list in input_file:
+    report=report_list.rstrip('\n').split(" ")
+    if isDecreasing(report) or isIncreasing(report):
+        for x in range(0,len(report) - 1):
+            difference = abs(int(report[x]) - int(report[x + 1]))
+            difference_per_level.append(difference)
+        difference_per_level.sort(reverse=True)
+        if difference_per_level[0] <= 3:
+            safe_reports += 1
+    difference_per_level.clear()
+print("Anzahl von sicheren Reports:",safe_reports)
