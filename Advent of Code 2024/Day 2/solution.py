f4b919a15aa693360146efe8a95ede0c9807f7be
Arnold Okkel <arnoldokkel121@gmail.com>
2024-12-07 10:52:05 +0100

    added parts of my solution in order to work on it later [not working]

--- Advent of Code 2024/Day 2/solution.py
+++ Advent of Code 2024/Day 2/solution.py
@@ -20,4 +20,43 @@ for report_list in input_file:
             safe_reports += 1
     difference_per_level.clear()
 print("Part 1: Anzahl von sicheren Reports:",safe_reports)
-#Part 1 more or less complete  . . .for unkown reasons the count of safe reports is 10% short of my answer
+#Part 1 complete after changing the casting method
+
+safe_reports = 0
+def isascending(list):
+    return all(list[index] <= list[index + 1] for index in range(0,len(list) - 1))
+
+def isdescending(list):
+    return all(list[index] >= list[index + 1] for index in range(0,len(list) - 1))
+
+temp_report = report.copy()
+    #checking ascending and descending reports seperately
+    if isascending(temp_report):
+        for x in range(0,len(report) - 2):
+            while removed == 0:
+                if report[x + 1] > report[x] and report[x + 2] < report[x + 1]:
+                    removed = 1
+                    temp_report.pop(x + 1)
+            if strictlyascending(temp_report):
+                for x in range(0,len(report) - 1):
+                    difference = abs(report[x] - report[x + 1])
+                    difference_per_level.append(difference)
+                    difference_per_level.sort(reverse=True)
+                if difference_per_level[0] <= 3:
+                    safe_reports += 1
+                difference_per_level.clear()
+    if isdescending(report):
+        for x in range(0,len(report) - 2):
+            while removed == 0:
+                if report[x + 1] < report[x] and report[x + 2] > report[x + 1]:
+                    removed = 1
+                    temp_report.pop(x + 1)
+            if strictlydescending(temp_report):
+                for x in range(0,len(report) - 1):
+                    difference = abs(report[x] - report[x + 1])
+                    difference_per_level.append(difference)
+                    difference_per_level.sort(reverse=True)
+                if difference_per_level[0] <= 3:
+                    safe_reports += 1
+                difference_per_level.clear()
+print ("Part 2: Anzahl sicherer Reports mit Dampener:",safe_reports)
