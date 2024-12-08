dc250d2444c6fab48e967db99e9021c74ca08803
Arnold Okkel <arnoldokkel121@gmail.com>
2024-12-08 07:43:48 +0100

    solution for part 2 is working now, but seems to be too low . . . W.I.P

--- Advent of Code 2024/Day 2/solution.py
+++ Advent of Code 2024/Day 2/solution.py
@@ -22,27 +22,62 @@ for report_list in input_file:
 print("Part 1: Anzahl von sicheren Reports:",safe_reports)
 #Part 1 complete after changing the casting method
 
-safe_reports = 0
-removed = 0
 
 def isascending(list):
     return all(list[index] <= list[index + 1] for index in range(0,len(list) - 1))
 
 def isdescending(list):
     return all(list[index] >= list[index + 1] for index in range(0,len(list) - 1))
-    #checking ascending and descending reports seperately
+
+safe_reports = 0
+input_file=open("input.txt","r")
+for report_list in input_file:
+    report=report_list.rstrip('\n').split(" ")
+    report = list(map(int, report))
+    removed = 0
+    x = 0
+    difference_per_level.clear()
+    while removed == 0 and x in range(0,len(report)):
+        buffer = report[x]
+        report.pop(x)
+        if strictlyascending(report) or strictlydescending(report):
+            for y in range(0,len(report) - 1):
+                difference = abs(report[y] - report[y + 1])
+                difference_per_level.append(difference)
+                difference_per_level.sort(reverse=True)
+            if difference_per_level[0] <= 3:
+                safe_reports += 1
+                removed == 1
+                x = len(report)
+                difference_per_level.clear()
+        if len(difference_per_level) >= len(report) -1:
+            x = len(report)
+        else: report.insert(x,buffer)
+        x += 1
+print ("Part 2: Anzahl sicherer Reports mit Dampener:",safe_reports)
+'''
 input_file=open("input.txt","r")    
 for report_list in input_file:
     report=report_list.rstrip('\n').split(" ")
     report = list(map(int, report))
-    if isascending(report):
-        for x in range(0,len(report) - 2):
-            if report[x + 1] > report[x] and report[x + 2] < report[x + 1]:
+    for x in range(0,len(report) - 2):
+        while removed == 0:
+            if report[x + 1] >= report[x] and report[x + 2] <= report[x + 1]:
                 removed = 1
                 report.pop(x + 1)
-            else: x += 1
-            removed = 0
-            if strictlyascending(report):
+            elif report[x + 1] <= report[x] and report[x + 2] >= report[x + 1]:
+                removed = 1
+                report.pop(x + 1)
+            elif report[x + 2] <= report[x + 1]
+        if strictlyascending(report):
+            for x in range(0,len(report) - 1):
+                difference = abs(report[x] - report[x + 1])
+                difference_per_level.append(difference)
+                difference_per_level.sort(reverse=True)
+            if difference_per_level[0] <= 3:
+                safe_reports += 1
+            difference_per_level.clear()
+            if strictlydescending(report):
                 for x in range(0,len(report) - 1):
                     difference = abs(report[x] - report[x + 1])
                     difference_per_level.append(difference)
@@ -50,18 +85,4 @@ for report_list in input_file:
                 if difference_per_level[0] <= 3:
                     safe_reports += 1
                 difference_per_level.clear()
-    if isdescending(report):
-        for x in range(0,len(report) - 2):
-            while removed == 0:
-                if report[x + 1] < report[x] and report[x + 2] > report[x + 1]:
-                    removed = 1
-                    temp_report.pop(x + 1)
-            if strictlydescending(temp_report):
-                for x in range(0,len(report) - 1):
-                    difference = abs(report[x] - report[x + 1])
-                    difference_per_level.append(difference)
-                    difference_per_level.sort(reverse=True)
-                if difference_per_level[0] <= 3:
-                    safe_reports += 1
-                difference_per_level.clear()
-print ("Part 2: Anzahl sicherer Reports mit Dampener:",safe_reports)
+'''
