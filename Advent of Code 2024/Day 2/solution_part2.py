4013b6034c547509c297966402548c3cbb5a0ed1
Arnold Okkel <arnoldokkel121@gmail.com>
2024-12-08 12:22:20 +0100

    applied logic from first attempt

--- Advent of Code 2024/Day 2/solution_part2.py
+++ Advent of Code 2024/Day 2/solution_part2.py
@@ -10,40 +10,29 @@ def strictlydescending(list):
 
 input_file=open("input.txt","r")
 
+safe_reports = 0
+input_file=open("input.txt","r")
 for report_list in input_file:
     report=report_list.rstrip('\n').split(" ")
     report = list(map(int, report))
+    removed = 0
     x = 0
     difference_per_level.clear()
-    while removed == False or x in range(0,len(report)):
+    while removed == 0 and x in range(0,len(report)):
+        buffer = report[x]
+        report.pop(x)
         if strictlyascending(report) or strictlydescending(report):
             for y in range(0,len(report) - 1):
                 difference = abs(report[y] - report[y + 1])
                 difference_per_level.append(difference)
-            difference_per_level.sort(reverse=True)
+                difference_per_level.sort(reverse=True)
             if difference_per_level[0] <= 3:
                 safe_reports += 1
-                removed = True
-                x = len(report) + 1
-            else: x += 1
-        elif x in range(0,len(report)):
-            buffer = report[x]
-            report.pop(x)
-            removed = True
-            if strictlyascending(report) or strictlydescending(report):
-                for y in range(0,len(report) - 1):
-                    difference = abs(report[y] - report[y + 1])
-                    difference_per_level.append(difference)
-                difference_per_level.sort(reverse=True)
-                if difference_per_level[0] <= 3:
-                    safe_reports += 1
-                    removed = True
-                    x = len(report) + 1
-            else:
-                report.insert(x,buffer)
-                x += 1
-                removed = False
-        else: 
-            x = len(report) +1
-            removed = True
+                removed == 1
+                x = len(report)
+                difference_per_level.clear()
+        if len(difference_per_level) >= len(report) -1:
+            x = len(report)
+        else: report.insert(x,buffer)
+        x += 1
 print ("Part 2: Anzahl sicherer Reports mit Dampener:",safe_reports)
